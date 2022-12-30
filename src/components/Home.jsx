1ddbcb4cc289306e917dc12d922301eb153bbc6b
naxucrishvili1993 <giorginaxucrishvili70@gmail.com>
2022-12-30 12:28:35 +0400

    Remove animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,33 +5,16 @@ const Home = () => {
 	return (
 		<section id="home">
 			<div className="home-left-section">
-				<div
-					data-aos="fade-right"
-					data-aos-delay="50"
-					data-aos-duration="500"
-					data-aos-easing="ease-in-out"
-					data-aos-mirror="true"
-					data-aos-once="true">
-					<h1>Hi, I'm Giorgi Nakhutsrishvili</h1>
-					<p>
-						I'm a <span>Junior Front-end Developer</span> with more than one
-						year of experience, focused on quality and clean code. With a
-						passion for both software development and personal growth, I
-						attended 100+ hour bootcamps to learn new technologies and
-						languages.
-					</p>
-				</div>
+				<h1>Hi, I'm Giorgi Nakhutsrishvili</h1>
+				<p>
+					I'm a <span>Junior Front-end Developer</span> with more than one year
+					of experience, focused on quality and clean code. With a passion for
+					both software development and personal growth, I attended 100+ hour
+					bootcamps to learn new technologies and languages.
+				</p>
 			</div>
 			<div className="home-right-section">
-				<div
-					data-aos="fade-left"
-					data-aos-delay="50"
-					data-aos-duration="500"
-					data-aos-easing="ease-in-out"
-					data-aos-mirror="true"
-					data-aos-once="true">
-					<img className="home-img" src={HomeIllustration} alt="Illustration" />
-				</div>
+				<img className="home-img" src={HomeIllustration} alt="Illustration" />
 			</div>
 		</section>
 	);
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,178 +10,137 @@ import Location from "../icons/location.svg";
 const Work = () => {
 	return (
 		<section id="work">
-			<div data-aos="fade-up" data-aos-duration="500" data-aos-once="true">
-				<div className="work-experience">
-					<h2>Work Experience:</h2>
-					<p>
-						Bellboy at{" "}
-						<a href="https://tiflispalace.ge/english/home">
-							Tiflis Palace Hotel
-						</a>
-					</p>
-					<ul>
-						<li>
-							<img src={Calendar} alt="Calendar" /> September 2021 - Present
-						</li>
-						<li>
-							<img src={Location} alt="Location" /> Tbilisi, Georgia
-						</li>
-					</ul>
-				</div>
-				<div className="certifications">
-					<h2>Certifications:</h2>
-					<ul>
-						<li>
-							Responsive Web Design -{" "}
-							<a href="https://www.freecodecamp.org/">FreeCodeCamp</a>
-						</li>
-						<li>
-							JavaScript Algorithms and Data Structures -{" "}
-							<a href="https://www.freecodecamp.org/">FreeCodeCamp</a>
-						</li>
-						<li>
-							The Complete 2022 Web Development Bootcamp -{" "}
-							<a href="https://www.udemy.com/">Udemy</a>
-						</li>
-					</ul>
-				</div>
-				<div className="skills">
-					<h2>Skills:</h2>
-					<ul className="skills-list">
-						<li>HyperText Markup Language (HTML5)</li>
-						<li>Cascading Style Sheets (CSS3)</li>
-						<li>Bootstrap</li>
-						<li>JavaScript</li>
-						<li>jQuery</li>
-						<li>React.js</li>
-						<li>Git</li>
-					</ul>
-				</div>
+			<div className="work-experience">
+				<h2>Work Experience:</h2>
+				<p>
+					Bellboy at{" "}
+					<a href="https://tiflispalace.ge/english/home">Tiflis Palace Hotel</a>
+				</p>
+				<ul>
+					<li>
+						<img src={Calendar} alt="Calendar" /> September 2021 - Present
+					</li>
+					<li>
+						<img src={Location} alt="Location" /> Tbilisi, Georgia
+					</li>
+				</ul>
+			</div>
+			<div className="certifications">
+				<h2>Certifications:</h2>
+				<ul>
+					<li>
+						Responsive Web Design -{" "}
+						<a href="https://www.freecodecamp.org/">FreeCodeCamp</a>
+					</li>
+					<li>
+						JavaScript Algorithms and Data Structures -{" "}
+						<a href="https://www.freecodecamp.org/">FreeCodeCamp</a>
+					</li>
+					<li>
+						The Complete 2022 Web Development Bootcamp -{" "}
+						<a href="https://www.udemy.com/">Udemy</a>
+					</li>
+				</ul>
+			</div>
+			<div className="skills">
+				<h2>Skills:</h2>
+				<ul className="skills-list">
+					<li>HyperText Markup Language (HTML5)</li>
+					<li>Cascading Style Sheets (CSS3)</li>
+					<li>Bootstrap</li>
+					<li>JavaScript</li>
+					<li>jQuery</li>
+					<li>React.js</li>
+					<li>Git</li>
+				</ul>
 			</div>
 			<div className="projects-section">
-				<div data-aos="fade-right" data-aos-duration="500" data-aos-once="true">
-					<h2>Projects:</h2>
-				</div>
+				<h2>Projects:</h2>
 				<div className="projects">
 					<div className="project">
-						<div
-							className="projects-animation-div"
-							data-aos="zoom-in-up"
-							data-aos-once="false"
-							data-aos-mirror="true"
-							data-aos-duration="500">
+						<a
+							href="https://naxucrishvili1993.github.io/youtube-clone/"
+							target="_blank"
+							rel="noreferrer">
+							<img
+								className="yt-logo"
+								src={FirstPhoto}
+								alt="Project Thumbnail"
+							/>
+						</a>
+						<h3>
 							<a
 								href="https://naxucrishvili1993.github.io/youtube-clone/"
 								target="_blank"
 								rel="noreferrer">
-								<img
-									className="yt-logo"
-									src={FirstPhoto}
-									alt="Project Thumbnail"
-								/>
+								YouTube Clone
 							</a>
-							<h3>
-								<a
-									href="https://naxucrishvili1993.github.io/youtube-clone/"
-									target="_blank"
-									rel="noreferrer">
-									YouTube Clone
-								</a>
-							</h3>
-						</div>
+						</h3>
 					</div>
 					<div className="project">
-						<div
-							className="projects-animation-div"
-							data-aos="zoom-in-up"
-							data-aos-once="false"
-							data-aos-mirror="true"
-							data-aos-duration="500">
+						<a
+							href="https://naxucrishvili1993.github.io/commerce-app/"
+							target="_blank"
+							rel="noreferrer">
+							<img src={SecondPhoto} alt="Project Thumbnail" />{" "}
+						</a>
+
+						<h3>
 							<a
 								href="https://naxucrishvili1993.github.io/commerce-app/"
 								target="_blank"
 								rel="noreferrer">
-								<img src={SecondPhoto} alt="Project Thumbnail" />{" "}
+								Shopping Cart
 							</a>
-
-							<h3>
-								<a
-									href="https://naxucrishvili1993.github.io/commerce-app/"
-									target="_blank"
-									rel="noreferrer">
-									Shopping Cart
-								</a>
-							</h3>
-						</div>
+						</h3>
 					</div>
 					<div className="project">
-						<div
-							className="projects-animation-div"
-							data-aos="zoom-in-up"
-							data-aos-once="false"
-							data-aos-mirror="true"
-							data-aos-duration="500">
+						<a
+							href="https://weather-app-react-inky.vercel.app/"
+							rel="noreferrer"
+							target="_blank">
+							<img src={ThirdPhoto} alt="Project Thumbnail" />
+						</a>
+						<h3>
 							<a
 								href="https://weather-app-react-inky.vercel.app/"
 								rel="noreferrer"
 								target="_blank">
-								<img src={ThirdPhoto} alt="Project Thumbnail" />
+								Weather App
 							</a>
-							<h3>
-								<a
-									href="https://weather-app-react-inky.vercel.app/"
-									rel="noreferrer"
-									target="_blank">
-									Weather App
-								</a>
-							</h3>
-						</div>
+						</h3>
 					</div>
 					<div className="project">
-						<div
-							className="projects-animation-div"
-							data-aos="zoom-in-up"
-							data-aos-once="false"
-							data-aos-mirror="true"
-							data-aos-duration="500">
+						<a
+							href="https://naxucrishvili1993.github.io/quiz-app/"
+							rel="noreferrer"
+							target="_blank">
+							<img src={FourthPhoto} alt="Project Thumbnail" />
+						</a>
+						<h3>
 							<a
 								href="https://naxucrishvili1993.github.io/quiz-app/"
 								rel="noreferrer"
 								target="_blank">
-								<img src={FourthPhoto} alt="Project Thumbnail" />
+								Quiz App
 							</a>
-							<h3>
-								<a
-									href="https://naxucrishvili1993.github.io/quiz-app/"
-									rel="noreferrer"
-									target="_blank">
-									Quiz App
-								</a>
-							</h3>
-						</div>
+						</h3>
 					</div>
 					<div className="project">
-						<div
-							className="projects-animation-div"
-							data-aos="zoom-in-up"
-							data-aos-once="false"
-							data-aos-mirror="true"
-							data-aos-duration="500">
+						<a
+							href="https://naxucrishvili1993.github.io/calculator/"
+							rel="noreferrer"
+							target="_blank">
+							<img src={FifthPhoto} alt="Project Thumbnail" />
+						</a>
+						<h3>
 							<a
 								href="https://naxucrishvili1993.github.io/calculator/"
 								rel="noreferrer"
 								target="_blank">
-								<img src={FifthPhoto} alt="Project Thumbnail" />
+								Calculator
 							</a>
-							<h3>
-								<a
-									href="https://naxucrishvili1993.github.io/calculator/"
-									rel="noreferrer"
-									target="_blank">
-									Calculator
-								</a>
-							</h3>
-						</div>
+						</h3>
 					</div>
 				</div>
 			</div>
